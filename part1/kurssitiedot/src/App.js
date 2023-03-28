c17fa94fdc72a377dee131689ebcdd0085bec189
Titta <titta.kemppi@gmail.com>
2023-03-28 14:07:51 +0300

    Task 1.1

--- part1/kurssitiedot/src/App.js
+++ part1/kurssitiedot/src/App.js
@@ -11,6 +11,27 @@ const Header = (props) => {
   )
 }
 
+const Content = (props) => {
+  return (
+    <div>
+      <p>
+        {props.part} {props.exercises}
+      </p>
+    </div>
+  )  
+
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>
+        Number of exercises {props.total}
+      </p>
+    </div>
+  )
+}
+
 
 const App = () => {
   const course = 'Half Stack application development'
@@ -24,16 +45,10 @@ const App = () => {
   return (
     <div>
       <Header course_name = {course} />
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Content part={part1} exercises = {exercises1} />
+      <Content part={part2} exercises = {exercises2} />
+      <Content part={part3} exercises = {exercises3} />
+      <Total total = {exercises1 + exercises2 + exercises3} />
     </div>
   )
 }

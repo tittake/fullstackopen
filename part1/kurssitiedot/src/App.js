56bd30fcbed1beb8a42f984d64210b889c015fcc
Titta <titta.kemppi@gmail.com>
2023-03-28 13:41:15 +0300

    First commit to test

---
+++ part1/kurssitiedot/src/App.js
@@ -0,0 +1,41 @@
+// header, content, total
+
+const Header = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <h1> 
+        {props.course_name}
+      </h1>
+    </div>
+  )
+}
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course_name = {course} />
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    </div>
+  )
+}
+
+export default App;
